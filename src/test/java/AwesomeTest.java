cbec1183b0b0f8e0290b42679d84c9c02055976a
MarianVizitiu <marin.vizzitiu@gmail.com>
2020-10-06 22:59:17 +0300

    added more awesome stuff

--- src/test/java/AwesomeTest.java
+++ src/test/java/AwesomeTest.java
@@ -3,4 +3,9 @@ public class AwesomeTest {
     //green means under staging area
     //grey in .idea folder it s ignored
     //when the color is normal it means commited no changes
+
+    // I just pulled the new change from GitHub that i did not have loclly
+    // and now my project is in sync with remote repository
+
+    //now I made more awesome changes and I Am ABOUT To push thios back to Github
 }
